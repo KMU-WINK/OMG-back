701a358ae588751dc7ed040a55635e49780614b0
cg10036 <cg10036@gmail.com>
2022-12-19 21:16:40 +0900

    change validate middleware name

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import authController from "../controllers/auth";
-import validate from "../validator";
+import validateMiddleware from "../validator";
 import { LoginValidator, RegisterValidator } from "../validator/auth";
 
 const router = express.Router();
 
-router.post("/login", validate(LoginValidator), authController.login);
-router.post("/register", validate(RegisterValidator), authController.register);
+router.post("/login", validateMiddleware(LoginValidator), authController.login);
+router.post(
+  "/register",
+  validateMiddleware(RegisterValidator),
+  authController.register
+);
 
 export default router;
