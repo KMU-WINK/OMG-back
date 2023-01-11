0d83a9d2d9e6f4e30f57a8eb425540e194e5a482
cg10036 <cg10036@gmail.com>
2023-01-11 15:47:23 +0900

    make sms module & sms code randomize

--- src/utils/sms.ts
+++ src/utils/sms.ts
@@ -1,7 +1,49 @@
+import crypto from "crypto";
+import fetch from "node-fetch";
+import config from "./config";
+
+const sign = (
+  method: string,
+  path: string,
+  timestamp: string,
+  accessKey: string,
+  secretKey: string
+) => {
+  return crypto
+    .createHmac("sha256", secretKey)
+    .update(`${method} ${path}\n${timestamp}\n${accessKey}`)
+    .digest("base64");
+};
+
 const sendSms = async (to: string, content: string) => {
-  console.log("DUMMY SMS MODULE!");
-  console.log({ to, content });
-  return true;
+  let path = `/sms/v2/services/${config.NCLOUD.serviceId}/messages`;
+  let method = "POST";
+  let timestamp = (+new Date()).toString();
+
+  let resp = await fetch(`https://sens.apigw.ntruss.com${path}`, {
+    method,
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+      "x-ncp-apigw-timestamp": timestamp,
+      "x-ncp-iam-access-key": config.NCLOUD.accessKey,
+      "x-ncp-apigw-signature-v2": sign(
+        method,
+        path,
+        timestamp,
+        config.NCLOUD.accessKey,
+        config.NCLOUD.secretKey
+      ),
+    },
+    body: JSON.stringify({
+      type: "SMS",
+      from: config.NCLOUD.from,
+      content: content,
+      messages: [{ to }],
+    }),
+  });
+  let json = await resp.json();
+
+  return json.statusName === "success";
 };
 
 export { sendSms };
