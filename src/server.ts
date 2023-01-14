d71b1a46596e94f515065f8b2e93e17112cb8b7d
cg10036 <cg10036@gmail.com>
2023-01-14 20:13:18 +0900

    add json paring error handler

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,17 @@ AppDataSource.initialize().then(() => {
         .status(400)
         .send({ code: "VALIDATION_ERROR", errors: err.errors });
     }
+    if (
+      err instanceof SyntaxError &&
+      (err as any).type === "entity.parse.failed" &&
+      err.message.indexOf("JSON") !== -1
+    ) {
+      return res.status(400).send({
+        code: "JSON_PARSE_FAILED",
+        detail:
+          "Failed to parse json. Please make sure your json syntax is correct",
+      });
+    }
     console.log(err);
     if (res.headersSent) {
       return res.end();
